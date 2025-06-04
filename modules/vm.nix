f3d351bd70812458bd6d4ffced2dd10bab08ab2e
Poly2it <84731064+Poly2it@users.noreply.github.com>
2025-06-04 03:36:41 +0200

    Enable Podman.

--- modules/vm.nix
+++ modules/vm.nix
@@ -16,5 +16,15 @@ in
   virtualisation.vmVariantWithBootLoader = {
     inherit virtualisation;
   };
+  virtualisation.podman = {
+    enable = true;
+    # Create the default bridge network for podman.
+    defaultNetwork.settings.dns_enabled = true;
+  };
+  boot.binfmt = {
+    emulatedSystems = [ "aarch64-linux" ];
+    # Required for Podman.
+    preferStaticEmulators = true;
+  };
 }
 
