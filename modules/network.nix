81be5abf095b519a5ee4c8c4cf6920b4698a3915
Poly2it <84731064+Poly2it@users.noreply.github.com>
2025-08-15 13:15:34 +0200

    Use Unbound, fix Tailscale with Mullvad

--- modules/network.nix
+++ modules/network.nix
@@ -4,12 +4,23 @@
   # https://community.torproject.org/relay/setup/exit/#dns-on-exit-relays
   # TOR recommends Unbound as one's DNS resolver.
   services.unbound = {
-    enable = false;
+    enable = true;
     settings = {
+      forward-zone = [
+        {
+          name = ".";
+          forward-tls-upstream = true;
+          forward-addr = "194.242.2.2@853#dns.mullvad.net";
+        }
+      ];
       server = {
         interface = ["127.0.0.1"];
-        port = 5335;
+        port = 53;
         access-control = ["127.0.0.1 allow"];
+
+        # verbosity = 1;
+        # log-queries = true;
+        # log-replies = true;
 
         # Based on recommended settings in https://docs.pi-hole.net/guides/dns/unbound/#configure-unbound
         harden-glue = true;
@@ -20,14 +31,16 @@
 
         hide-identity = true;
         hide-version = true;
+
+        do-not-query-localhost = false;
+        edns-tcp-keepalive = true;
       };
     };
   };
 
-  services.resolved = {
-    enable = true;
-    dnssec = "true";
-  };
+  networking.nameservers = [ "127.0.0.1" ];
+
+  services.resolved.enable = false;
 
   networking.resolvconf.useLocalResolver = true;
 
@@ -37,17 +50,40 @@
     wifi.backend = "iwd";
   };
 
-  networking.firewall.allowedTCPPorts = [
-    # I2P.
-    4444
-    4445
-    4447
-    7656
-    7657
-    7658
-    7659
-    7660
-  ];
+  networking.nftables = {
+    enable = true;
+    tables = {
+      mullvad_tailscale = {
+        name = "mullvad_tailscale";
+        family = "inet";
+        content = ''
+          chain output {
+            type route hook output priority -100; policy accept;
+            ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+
+          chain input {
+            type filter hook input priority -100; policy accept;
+            ip saddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+        '';
+      };
+    };
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [
+      # I2P.
+      4444
+      4445
+      4447
+      7656
+      7657
+      7658
+      7659
+      7660
+    ];
+  };
 
   services.i2pd = rec {
     enable = true;
